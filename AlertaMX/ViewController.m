2afcaa3ac5cb5b04f9d7258f4c1d279851f1590b
Walter <walter.gonzalez@smartplace.mx>
2014-09-04 15:39:04 -0500

    Initial Commit

---
+++ AlertaMX/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AlertaMX
+//
+//  Created by Walter on 04/09/14.
+//  Copyright (c) 2014 Smartplace. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ AlertaMXTests/AlertaMXTests.m
@@ -0,0 +1,34 @@
+//
+//  AlertaMXTests.m
+//  AlertaMXTests
+//
+//  Created by Walter on 04/09/14.
+//  Copyright (c) 2014 Smartplace. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AlertaMXTests : XCTestCase
+
+@end
+
+@implementation AlertaMXTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
